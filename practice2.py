491e36f214a2b0b6533fc6384f96a06b34329db3
alan88888 <45704365+alan88888@users.noreply.github.com>
2022-09-15 10:48:33 +0800

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,6 @@
+money=11805
+day_rate=0.0005
+month1=150
+month2=300
+sum_money=money*(1+day_rate)**59+month1+month2
+print("總共償還金額：",int(sum_money),"元")
---
+++ practice3.py
@@ -0,0 +1,9 @@
+event=input("輸入訊息\n")
+if event == "被告闖紅燈":
+    print("被告賠100%")
+elif event =="原告闖紅燈":
+    print("原告多被告少")
+elif event =="被告原告皆闖紅燈":
+    print("原告50%被告50%")
+else :
+    print("請填入有效訊息")    
