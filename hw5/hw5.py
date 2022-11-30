89d28c9de2ba574f2a83ba497cb036a58d255797
alan88888 <45704365+alan88888@users.noreply.github.com>
2022-12-01 00:19:49 +0800

    Update hw5.py

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,3 +1,9 @@
+import os
+#os.system("pip install scipy")
+os.system("pip install plotly==5.11.0")
+#os.system("pip install networkx")
+#os.system("pip install jieba")
+#os.system("pip install matplotlib")
 import jieba as jb
 import networkx as nx
 from collections import Counter
@@ -6,31 +12,19 @@ from wordcloud import WordCloud
 import scipy as sp
 import pandas as pd
 import jieba.posseg as pseg
-import os
-#os.system("pip install scipy")
-#os.system("pip install networkx")
-#os.system("pip install jieba")
-#os.system("pip install matplotlib")
+import plotly.express as px
+
 jb.set_dictionary('dict.txt.big')
 with open ('stops.txt','r',encoding='utf-8') as st:
     stops=st.read().split('\n')
     stops.append('\n')  
     stops.append('\n\n')
-    stops.append('。」')
-    stops.append('羅斯')
-    stops.append('克蘭')
-    stops.append('已經')
-    stops.append('赫爾')
-    stops.append('克里')
-    stops.append('里米')
-    stops.append('米亞')
-    stops.append('亞運')
 with open('hw5.txt','r',encoding='utf-8') as text:
     teststr=text.read()
 terms = [t for t in jb.cut(teststr, cut_all=True) if t not in stops]
 counter=sorted(Counter(terms).items(), key=lambda x:x[1], reverse=True)
+counter=counter[:20]
 data = pd.DataFrame(counter)
-print(data)
 for i in range(len(data[0])):
   for j in list(jb.cut(data[0][i],cut_all=False)):
     if j not in stops:
@@ -53,4 +47,8 @@ for sentence in toprow:
             term_dic[term] = {}
 matrix = pd.DataFrame.from_dict(term_dic)
 comatrix = matrix.T.dot(matrix)
-print(comatrix)
+import plotly.express as px
+fig = px.imshow(comatrix)
+fig.layout.height = 1000
+fig.layout.width = 1000
+fig.show()
