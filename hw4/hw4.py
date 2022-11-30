2e73a6fcaf7970a91d579325fbe1b57706b4287f
alan88888 <45704365+alan88888@users.noreply.github.com>
2022-11-30 10:26:48 +0800

    Update hw4.py

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,4 +1,5 @@
 import jieba as jb
+import networkx as nx
 from collections import Counter
 from matplotlib import pyplot as mt
 from wordcloud import WordCloud
@@ -7,7 +8,12 @@ with open ('stops.txt','r',encoding='utf-8') as st:
     stops=st.read().split('\n')
     stops.append('\n')  
     stops.append('\n\n')
-#teststr='''中文自然語言處理，與英文最大的差別就在斷詞，但是說實話，這個部分至今仍然沒有一個套件可以做好很好。目前而言，繁體中文有兩個套件可以使用，一個是中研院開發的斷詞系統，但是經過多方打聽，使用上並不是特別方便。因此我個人打選擇使用第二套系統jieba，中文叫做結巴，很幸運地這個套件有python的介面，使用上非常容易，只是這是大陸人開發的系統，必須自行輸入繁體字典，這篇文章的繁體字典出處在這邊。順帶一提，這篇文章寫得很好，對於jieba如果有想要有更深的認識，可以好好拜讀。'''
+    stops.append('羅斯')
+    stops.append('克蘭')
+    stops.append('已經')
+    stops.append('赫爾')
+    stops.append('克里')
+    stops.append('里米')
 with open('hw4.txt','r',encoding='utf-8') as text:
     teststr=text.read()
 terms = [t for t in jb.cut(teststr, cut_all=True) if t not in stops]
